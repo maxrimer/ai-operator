920130a1b9c0179f2c44f17726a7951d5f1dfdbe
Yeraly Shaken <ereke.shaken@gmail.com>
2025-06-14 11:48:05 +0500

    generate_hint

--- src/agent/langgraph_agent.py
+++ src/agent/langgraph_agent.py
@@ -146,17 +146,17 @@ def generate_hint(state: CallState) -> CallState:
         content = content + "}"
     try:
         final = json.loads(content)
-        state.hint = final["hint"]
-        state.confidence = final["confidence"]
-        state.source = final["source"]
-        agent_reply = AIMessage(content=final["hint"])
+        state.hint = final.get("hint")
+        state.confidence = final.get("confidence")
+        state.source = final.get("source")
+        agent_reply = AIMessage(content=final.get("hint"))
         state.messages.append(agent_reply)
         logger.info(f'Finished #3 State: {final}')
     except json.JSONDecodeError:
         try:
             final = ast.literal_eval(content)
         except Exception:
-            raise ValueError(f"Bad LLM output, cannot parse JSON:\n{content}")
+            logger.error(f"Bad LLM output, cannot parse JSON:\n{content}")
 
     return state
 
